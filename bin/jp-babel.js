c19652a01e5c08e3063e98861f876d9974c87190
Nicolas Riesco <enquiries@nicolasriesco.net>
2017-04-13 22:08:09 +0100

    bin: Match command flags in IJavascript
    * Added flag --jp-show-undefined to show undefined results.
    
    * Added flag --jp-spec-path=[none|full] to set whether kernel spec uses
      full paths.
    
    * Added flag --jp-startup-script=path to run startup scripts.
    
    * Added flag --versions to show jp-Babel and library versions.

--- bin/jp-babel.js
+++ bin/jp-babel.js
@@ -33,133 +33,91 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
-var DEBUG = false;
 
 var console = require("console");
-var crypto = require("crypto");
-var exec = require("child_process").exec;
 var fs = require("fs");
-var os = require("os");
-var path = require("path");
-var spawn = require("child_process").spawn;
 var util = require("util");
 
-var usage = (
-    "Babel Notebook\n" +
-    "\n" +
-    "Usage:\n" +
-    "\n" +
-    "    jp-babel <options>\n" +
-    "\n" +
-    "The recognised options are:\n" +
-    "\n" +
-    "    --jp-debug                   enable debug log level\n" +
-    "    --jp-help                    show this help\n" +
-    "    --jp-hide-undefined          do not show undefined results\n" +
-    "    --jp-install=[local|global]  install Babel kernel\n" +
-    "    --jp-install-kernel          same as --jp-install=local\n" +
-    "                                 (for backwards-compatibility)\n" +
-    "    --jp-protocol=version  set protocol version, e.g. 4.1\n" +
-    "    --jp-working-dir=path  set Babel working directory\n" +
-    "                           (default = current working directory)\n" +
-    "    --version              show Babel version\n" +
-    "\n" +
-    "and any other options recognised by the Jupyter notebook; run:\n" +
-    "\n" +
-    "    jupyter notebook --help\n" +
-    "\n" +
-    "for a full list.\n"
-);
+var rc = require("./rc.js");
+var context = rc.context;
+var copyAsync = rc.copyAsync;
+var doLog = rc.doLog;
+var dontLog = rc.dontLog;
+var getPackageVersion = rc.getPackageVersion;
+var installKernelAsync = rc.installKernelAsync;
+var log = rc.log;
+var makeTmpdir = rc.makeTmpdir;
+var readPackageJson = rc.readPackageJson;
+var setIPythonInfoAsync = rc.setIPythonInfoAsync;
+var setJupyterInfoAsync = rc.setJupyterInfoAsync;
+var setPaths = rc.setPaths;
+var setProtocol = rc.setProtocol;
+var spawnFrontend = rc.spawnFrontend;
 
-/**
- * @typedef Context
- *
- * @property            context
- * @property            context.path
- * @property {String}   context.path.node     Path to Node.js shell
- * @property {String}   context.path.root     Path to Babel root folder
- * @property {String}   context.path.kernel   Path to Babel kernel
- * @property {String}   context.path.images   Path to Babel images folder
- * @property {Object}   context.packageJSON   Contents of npm package.json
- * @property            context.flag
- * @property {Boolean}  context.flag.debug    --jp-debug
- * @property {String}   context.flag.install  --jp-install=[local|global]
- * @property {String}   context.flag.cwd      --jp-working-dir=path
- * @property            context.args
- * @property {String[]} context.args.kernel   Command arguments to run kernel
- * @property {String[]} context.args.frontend Command arguments to run frontend
- * @property            context.protocol
- * @property {String}   context.protocol.version      Protocol version
- * @property {Integer}  context.protocol.majorVersion Protocol major version
- * @property            context.frontend
- * @property {String}   context.frontend.version      Frontend version
- * @property {Integer}  context.frontend.majorVersion Frontend major version
- */
-
-/**
- * Script context
- * @type Context
- */
-var context = {
-    path: {},
-    packageJSON: undefined,
-    flag: {},
-    args: {},
-    protocol: {},
-    frontend: {},
-};
-
-setPaths(context);
-readPackageJson(context);
-parseCommandArgs(context);
-setFrontendInfoAsync(context, function() {
-    setProtocol(context);
-
-    if (DEBUG) console.log("CONTEXT:", context);
-
-    installKernelAsync(context, function() {
-        if (!context.flag.install) {
-            spawnFrontend(context);
-        }
-    });
-});
-
-function setPaths(context) {
-    context.path.node = process.argv[0];
-    context.path.root = path.dirname(path.dirname(
-        fs.realpathSync(process.argv[1])
-    ));
-    context.path.kernel = path.join(context.path.root, "lib", "kernel.js");
-    context.path.images = path.join(context.path.root, "images");
-}
-
-function readPackageJson(context) {
-    context.packageJSON = JSON.parse(
-        fs.readFileSync(path.join(context.path.root, "package.json"))
-    );
-}
+var usage = [
+    "Babel Notebook",
+    "",
+    "Usage:",
+    "",
+    "    jp-babel <options>",
+    "",
+    "The recognised options are:",
+    "",
+    "    --help                       show jp-Babel and notebook help",
+    "    --jp-debug                   enable debug log level",
+    "    --jp-help                    show this help",
+    "    --jp-hide-undefined          do not show undefined results",
+    "    --jp-install=[local|global]  install jp-Babel kernel",
+    "    --jp-install-kernel          same as --jp-install=local",
+    "                                 (for backwards-compatibility)",
+    "    --jp-protocol=version        set protocol version, e.g. 5.0",
+    "    --jp-show-undefined          show undefined results",
+    "    --jp-spec-path=[none|full]   set whether kernel spec uses full paths",
+    "    --jp-startup-script=path     run script on startup",
+    "                                 (path can be a file or a folder)",
+    "    --jp-working-dir=path        set session working directory",
+    "                                 (default = current working directory)",
+    "    --version                    show jp-Babel version",
+    "    --versions                   show jp-Babel and library versions",
+    "",
+    "and any other options recognised by the Jupyter notebook; run:",
+    "",
+    "    jupyter notebook --help",
+    "",
+    "for a full list.",
+].join("\n");
 
 function parseCommandArgs(context) {
-    context.args.kernel = [
-        context.path.node,
-        context.path.kernel,
-    ];
+    context.args.kernel = [];
     context.args.frontend = [
-        "ipython",
+        "jupyter",
         "notebook",
     ];
+    context.flag.hideUndefined = true;
 
+    var FLAG_HELP = "--help";
+    var FLAG_JP_HELP = "--jp-help";
+    var FLAG_JP_DEBUG = "--jp-debug";
+    var FLAG_JP_HIDE_UNDEFINED = "--jp-hide-undefined";
+    var FLAG_JP_INSTALL = "--jp-install=";
+    var FLAG_JP_INSTALL_KERNEL = "--jp-install-kernel";
+    var FLAG_JP_PROTOCOL = "--jp-protocol=";
+    var FLAG_JP_SHOW_UNDEFINED = "--jp-show-undefined";
+    var FLAG_JP_SPEC_PATH = "--jp-spec-path=";
+    var FLAG_JP_STARTUP_SCRIPT = "--jp-startup-script=";
+    var FLAG_JP_WORKING_DIR = "--jp-working-dir=";
+    var FLAG_VERSION = "--version";
+    var FLAG_VERSIONS = "--versions";
     process.argv.slice(2).forEach(function(e) {
-        var FLAG_JP_DEBUG = "--jp-debug";
-        var FLAG_JP_HELP = "--jp-help";
-        var FLAG_JP_HIDE_UNDEFINED = "--jp-hide-undefined";
-        var FLAG_JP_INSTALL = "--jp-install=";
-        var FLAG_JP_INSTALL_KERNEL = "--jp-install-kernel";
-        var FLAG_JP_PROTOCOL = "--jp-protocol=";
-        var FLAG_JP_WORKING_DIR = "--jp-working-dir=";
+        if (e === FLAG_HELP) {
+            console.log(usage);
+            context.args.frontend.push(e);
 
-        if (e === FLAG_JP_DEBUG) {
-            context.flag.debug = DEBUG = true;
+        } else if (e === FLAG_JP_DEBUG) {
+            DEBUG = true;
+            log = doLog;
+
+            context.flag.debug = true;
             context.args.kernel.push("--debug");
 
         } else if (e === FLAG_JP_HELP) {
@@ -167,7 +125,7 @@ function parseCommandArgs(context) {
             process.exit(0);
 
         } else if (e === FLAG_JP_HIDE_UNDEFINED) {
-            context.args.kernel.push("--hide-undefined");
+            context.flag.hideUndefined = true;
 
         } else if (e.lastIndexOf(FLAG_JP_INSTALL, 0) === 0) {
             context.flag.install = e.slice(FLAG_JP_INSTALL.length);
@@ -189,6 +147,23 @@ function parseCommandArgs(context) {
                 context.protocol.version.split(".", 1)[0]
             );
 
+        } else if (e === FLAG_JP_SHOW_UNDEFINED) {
+            context.flag.hideUndefined = false;
+
+        } else if (e.lastIndexOf(FLAG_JP_SPEC_PATH, 0) === 0) {
+            context.flag.specPath = e.slice(FLAG_JP_SPEC_PATH.length);
+            if (context.flag.specPath !== "none" &&
+                context.flag.specPath !== "full") {
+                console.error(
+                    util.format("Error: Unknown flag option '%s'\n", e)
+                );
+                console.error(usage);
+                process.exit(1);
+            }
+
+        } else if (e.lastIndexOf(FLAG_JP_STARTUP_SCRIPT, 0) === 0) {
+            context.flag.startup = e.slice(FLAG_JP_STARTUP_SCRIPT.length);
+
         } else if (e.lastIndexOf(FLAG_JP_WORKING_DIR, 0) === 0) {
             context.flag.cwd = fs.realpathSync(
                 e.slice(FLAG_JP_WORKING_DIR.length)
@@ -202,8 +177,17 @@ function parseCommandArgs(context) {
         } else if (e.lastIndexOf("--KernelManager.kernel_cmd=", 0) === 0) {
             console.warn(util.format("Warning: Flag '%s' skipped", e));
 
-        } else if (e === "--version") {
+        } else if (e === FLAG_VERSION) {
             console.log(context.packageJSON.version);
+            process.exit(0);
+
+        } else if (e === FLAG_VERSIONS) {
+            console.log("jp-babel", context.packageJSON.version);
+            console.log("jmp", getPackageVersion("jmp"));
+            console.log("jp-kernel", getPackageVersion("jp-kernel"));
+            console.log("nel", getPackageVersion("nel"));
+            console.log("uuid", getPackageVersion("uuid"));
+            console.log("zeromq", getPackageVersion("zeromq"));
             process.exit(0);
 
         } else {
@@ -211,177 +195,49 @@ function parseCommandArgs(context) {
         }
     });
 
+    if (context.flag.specPath === "full") {
+        context.args.kernel = [
+            context.path.node,
+            context.path.kernel,
+        ].concat(context.args.kernel);
+    } else {
+        context.args.kernel = [
+            (process.platform === 'win32') ? 'jp-babel-kernel.cmd' : 'jp-babel-kernel',
+        ].concat(context.args.kernel);
+    }
+
+    if (context.flag.startup) {
+        context.args.kernel.push("--startup-script=" + context.flag.startup);
+    }
+
     if (context.flag.cwd) {
         context.args.kernel.push("--session-working-dir=" + context.flag.cwd);
+    }
+
+    if (context.flag.hideUndefined) {
+        context.args.kernel.push("--hide-undefined");
+    } else {
+        context.args.kernel.push("--show-undefined");
     }
 
     context.args.kernel.push("{connection_file}");
 }
 
-function setFrontendInfoAsync(context, callback) {
-    exec("ipython --version", function(error, stdout, stderr) {
-        if (error) {
-            console.error("Error running `ipython --version`");
-            console.error(error.toString());
-            if (stderr) console.error(stderr.toString());
-            if (DEBUG) console.log("CONTEXT:", context);
-            process.exit(1);
-        }
 
-        context.frontend.version = stdout.toString().trim();
-        context.frontend.majorVersion = parseInt(
-            context.frontend.version.split(".")[0]
-        );
-        if (isNaN(context.frontend.majorVersion)) {
-            console.error(
-                "Error parsing IPython version:",
-                context.version.frontend
-            );
-            if (DEBUG) console.log("CONTEXT:", context);
-            process.exit(1);
-        }
+setPaths(context);
 
-        if (callback) {
-            callback();
+readPackageJson(context);
+
+parseCommandArgs(context);
+
+setJupyterInfoAsync(context, function() {
+    setProtocol(context);
+
+    installKernelAsync(context, function() {
+        log("CONTEXT:", context);
+
+        if (!context.flag.install) {
+            spawnFrontend(context);
         }
     });
-}
-
-function setProtocol(context) {
-    if (!context.protocol.version) {
-        if (context.frontend.majorVersion < 3) {
-            context.protocol.version = "4.1";
-            context.protocol.majorVersion = 4;
-        } else {
-            context.protocol.version = "5.0";
-            context.protocol.majorVersion = 5;
-        }
-    }
-
-    context.args.kernel.push("--protocol=" + context.protocol.version);
-
-    if (context.frontend.majorVersion < 3) {
-        context.args.frontend.push(util.format(
-            "--KernelManager.kernel_cmd=['%s']",
-            context.args.kernel.join("', '")
-        ));
-    }
-
-    if (context.frontend.majorVersion < 3 &&
-        context.protocol.majorVersion >= 5) {
-        console.warn("Warning: Protocol v5+ requires IPython v3+");
-    }
-}
-
-function installKernelAsync(context, callback) {
-    if (context.frontend.majorVersion < 3) {
-        if (context.flag.install) {
-            console.error(
-                "Error: Installation of kernel specs requires IPython v3+"
-            );
-        }
-
-        if (callback) {
-            callback();
-        }
-
-        return;
-    }
-
-    // Create temporary spec folder
-    var tmpdir = makeTmpdir();
-    var specDir = path.join(tmpdir, "babel");
-    fs.mkdirSync(specDir);
-
-    // Create spec file
-    var specFile = path.join(specDir, "kernel.json");
-    var spec = {
-        argv: context.args.kernel,
-        display_name: "Babel (Node.js)",
-        language: "babel",
-    };
-    fs.writeFileSync(specFile, JSON.stringify(spec));
-
-    // Copy logo files
-    var logo32Src = path.join(context.path.images, "logo-32x32.png");
-    var logo32Dst = path.join(specDir, "logo-32x32.png");
-    var logo64Src = path.join(context.path.images, "logo-64x64.png");
-    var logo64Dst = path.join(specDir, "logo-64x64.png");
-    copyAsync(logo32Src, logo32Dst, function() {
-        copyAsync(logo64Src, logo64Dst, function() {
-
-            // Install kernel spec
-            var cmd = "ipython kernelspec install --replace " + specDir;
-            if (context.flag.install !== "global") {
-                cmd += "  --user";
-            }
-            exec(cmd, function(error, stdout, stderr) {
-
-                // Remove temporary spec folder
-                fs.unlinkSync(specFile);
-                fs.unlinkSync(logo32Dst);
-                fs.unlinkSync(logo64Dst);
-                fs.rmdirSync(specDir);
-                fs.rmdirSync(tmpdir);
-
-                if (error) {
-                    console.error(util.format("Error running `%s`", cmd));
-                    console.error(error.toString());
-                    if (stderr) console.error(stderr.toString());
-                    if (DEBUG) console.log("CONTEXT:", context);
-                    process.exit(1);
-                }
-
-                if (callback) {
-                    callback();
-                }
-            });
-        });
-    });
-}
-
-function spawnFrontend(context) {
-    var cmd = context.args.frontend[0];
-    var args = context.args.frontend.slice(1);
-    var frontend = spawn(cmd, args, {
-        stdio: "inherit"
-    });
-
-    // Relay SIGINT onto the frontend
-    var signal = "SIGINT";
-    process.on(signal, function() {
-        frontend.emit(signal);
-    });
-}
-
-function makeTmpdir(maxAttempts) {
-    maxAttempts = maxAttempts ? maxAttempts : 10;
-    var attempts = 0;
-
-    var tmpdir;
-    while (!tmpdir) {
-        attempts++;
-        try {
-            tmpdir = path.join(
-                os.tmpdir(),
-                crypto.randomBytes(16).toString('hex')
-            );
-            fs.mkdirSync(tmpdir);
-        } catch (e) {
-            if (attempts >= maxAttempts)
-                throw e;
-            tmpdir = null;
-        }
-    }
-
-    return tmpdir;
-}
-
-function copyAsync(src, dst, callback) {
-    var readStream = fs.createReadStream(src);
-    var writeStream = fs.createWriteStream(dst);
-    if (callback) {
-        readStream.on("end", callback);
-    }
-    readStream.pipe(writeStream);
-}
+});
